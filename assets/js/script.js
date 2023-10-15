32d89e6e34e9de480e07582045bf5410386a409e
Alex M <alexmajors0613@gmail.com>
2023-10-15 05:39:43 -0500

    add getWeather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,27 +186,18 @@ function getCoords(event) {
   }
   var city = input[0];
   if (input[1]) {
-    var country = ", " + input[1];
+    var country = "," + input[1];
   } else {
     var country = "";
   }
-
   fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-      city +
-      country +
-      "&limit=1&appid=" +
-      apiKey
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}${country}&limit=1&appid=${apiKey}`
   )
     .then((response) => {
       if (response.ok) {
         console.log(
-          "URL:",
-          "http://api.openweathermap.org/geo/1.0/direct?q=" +
-            city +
-            country +
-            "&limit=1&appid=" +
-            apiKey
+          "City URL:",
+          `http://api.openweathermap.org/geo/1.0/direct?q=${city}${country}&limit=1&appid=${apiKey}`
         );
         return response.json();
       } else {
@@ -214,15 +205,94 @@ function getCoords(event) {
       }
     })
     .then((data) => {
-      console.log("Data:", data[0]);
+      console.log("City Data:", data[0]);
       var lat = data[0].lat;
       var lon = data[0].lon;
       console.log("Latitude:", lat, " Longitude:", lon);
-      console.log(data);
-      if ($(this).attr("id") == "citySearch") {
-        saveCity(city, country);
+      saveCity(city, country.replaceAll(",", ", "));
+      getWeather(lat, lon);
+    });
+}
+
+function getWeather(lat, lon) {
+  //fetches the weather data for a given latitude and longitude
+  fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+  )
+    .then((response) => {
+      if (response.ok) {
+        console.log(
+          "Weather URL:",
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+        );
+        return response.json();
+      } else {
+        //TODO: Error message on not response.ok
       }
-      // getWeather(lat, lon);
+    })
+    .then((data) => {
+      console.log("Weather Data:", data);
+      console.log(
+        //logs the current weather data and and the date
+        "Today:",
+        data.list[0],
+        dayjs.unix(data.list[0].dt).format("MM/DD/YYYY")
+      );
+      $("#currentWeather") //changes the currentWeather header to the city name and date
+        .children("h2")
+        .text(
+          data.city.name +
+            " " +
+            dayjs.unix(data.list[0].dt).format("(MM/DD/YYYY)")
+        );
+      $("#currentWeather") //changes the currentWeather img to the current weather icon
+        .children("img")
+        .attr({
+          src: `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`,
+          alt: data.list[0].weather[0].main + " weather icon",
+        });
+      $("#currentWeather") //changes the currentWeather temp text to the temperature
+        .children(".temp")
+        .text("Temp: " + data.list[0].main.temp + "9\xB0F");
+      $("#currentWeather") //changes the currentWeather wind to the wind speed
+        .children(".wind")
+        .text("Wind: " + data.list[0].wind.speed + "MPH");
+      $("#currentWeather") //changes the currentWeather humid to the humidity
+        .children(".humid")
+        .text("Humidity: " + data.list[0].main.humidity + "%");
+      /* for (i = 7; i < data.list.length; i += 8) */
+      $("#forecast")
+        .children()
+        .each(function (i, val) {
+          var dayIndex = 7 + i * 8; //the index for the weather data of the date at the current time
+          var date = dayjs.unix(data.list[dayIndex].dt).format("MM/DD/YYYY"); //current date formated
+          console.log(
+            //logs the weather data and and the date for each day
+            "Day " + (i + 2) + ":",
+            data.list[dayIndex],
+            date,
+            dayIndex,
+            val
+          );
+          $(val) //changes the day's forecast header to the city name
+            .children("h5")
+            .text(dayjs.unix(data.list[0].dt).format("MM/DD/YYYY"));
+          $(val) //changes the day's forecast img to the current weather icon
+            .children("img")
+            .attr({
+              src: `https://openweathermap.org/img/wn/${data.list[dayIndex].weather[0].icon}@2x.png`,
+              alt: data.list[dayIndex].weather[0].main + " weather icon",
+            });
+          $(val) //changes the day's forecast temp text to the temperature
+            .children(".temp")
+            .text("Temp: " + data.list[dayIndex].main.temp + "9\xB0F");
+          $(val) //changes the day's forecast wind to the wind speed
+            .children(".wind")
+            .text("Wind: " + data.list[dayIndex].wind.speed + "MPH");
+          $(val) //changes the day's forecast humid to the humidity
+            .children(".humid")
+            .text("Humidity: " + data.list[dayIndex].main.humidity + "%");
+        });
     });
 }
 
@@ -260,5 +330,6 @@ function getSavedCities() {
 }
 
 getSavedCities();
+getWeather(0, 0);
 $("#citySearch").on("submit", getCoords);
 $(".savedCity").on("click", getCoords);
